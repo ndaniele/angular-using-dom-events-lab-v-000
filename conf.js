2fcb900ba119c6f7b4a7a282871f3d754038b21d
ndaniele <n.daniele@gmail.com>
2016-11-06 17:19:23 -0500

    Done.

--- conf.js
+++ conf.js
@@ -1,5 +1,8 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: ['spec/server.js', 'spec/**/*.spec.js'],
-	rootElement: '.app'
+	rootElement: '.app',
+    capabilities: {
+      'browserName': 'firefox'
+    },
 };
